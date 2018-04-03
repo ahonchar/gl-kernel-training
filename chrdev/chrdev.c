71659c6949637006930478f648d2be2acc65503b
Andriy Khulap <andriy.khulap@globallogic.com>
2018-04-03 18:16:53 +0300

    chrdev: add simple read-back characted device
    Signed-off-by: Andriy Khulap <andriy.khulap@globallogic.com>

--- chrdev/chrdev.c
+++ chrdev/chrdev.c
@@ -2,15 +2,101 @@
 #include <linux/module.h>
 #include <linux/device.h>
 #include <linux/err.h>
+#include <linux/uaccess.h>
+
+#define DEVICE_NAME	"chrdev_example"
+#define BUFFER_SIZE	1024
+
+static int major;
+static int is_open;
+
+static int data_size;
+static unsigned char data_buffer[BUFFER_SIZE];
+
+static int dev_open(struct inode *inodep, struct file *filep)
+{
+	if (is_open) {
+		pr_err("chrdev: already open\n");
+		return -EBUSY;
+	}
+
+	is_open = 1;
+	pr_info("chrdev: device opened\n");
+	return 0;
+}
+
+static int dev_release(struct inode *inodep, struct file *filep)
+{
+	is_open = 0;
+	pr_info("chrdev: device closed\n");
+	return 0;
+}
+
+
+static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset)
+{
+	int ret;
+
+	if (len > data_size) len = data_size;
+
+	ret = copy_to_user(buffer, data_buffer, len);
+	if (ret) {
+		pr_err("chrdev: copy_to_user failed: %d\n", ret);
+		return -EFAULT;
+	}
+	data_size = 0; /* eof for cat */
+
+	pr_info("chrdev: %ld bytes read\n", len);
+	return len;
+}
+
+static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset)
+{
+	int ret;
+
+	data_size = len;
+	if (data_size > BUFFER_SIZE) data_size = BUFFER_SIZE;
+
+	ret = copy_from_user(data_buffer, buffer, data_size);
+	if (ret) {
+		pr_err("chrdev: copy_from_user failed: %d\n", ret);
+		return -EFAULT;
+	}
+
+	pr_info("chrdev: %d bytes written\n", data_size);
+	return data_size;
+}
+
+
+
+static struct file_operations fops =
+{
+	.open = dev_open,
+	.release = dev_release,
+	.read = dev_read,
+	.write = dev_write,
+};
 
 static int chrdev_init(void)
 {
+	is_open = 0;
+	data_size = 0;
+
+	major = register_chrdev(0, DEVICE_NAME, &fops);
+	if (major < 0) {
+		pr_err("register_chrdev failed: %d\n", major);
+		return major;
+	}
+	pr_info("chrdev: register_chrdev ok, major = %d\n", major);
+	
 	pr_info("chrdev: module loaded\n");
 	return 0;
 }
 
 static void chrdev_exit(void)
 {
+	unregister_chrdev(major, DEVICE_NAME);
+
 	pr_info("chrdev: module exited\n");
 }
 
